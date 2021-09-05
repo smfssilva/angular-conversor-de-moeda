8a4cbf56da3fa69b429e23b29a6d4f01cbe67854
Silva <smfssilva@yahoo.com>
2021-09-05 20:47:52 -0300

    Implementando o servico gerenciamento de moeda

--- src/app/conversor/services/moeda.service.ts
+++ src/app/conversor/services/moeda.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
 
+import { Moeda } from "../models";
+
 @Injectable({
   providedIn: 'root'
 })
 export class MoedaService {
 
+  private moedas: Moeda[];
+
   constructor() { }
+
+  private moedasObj = [
+
+  ];
+
+  listarTodas(): Moeda[] {
+    if (this.moedas) {
+      return this.moedas;
+    }
+
+    this.moedas = [];
+
+    for (let moedaObj of this.moedasObj) {
+      let moeda: Moeda = new Moeda();
+      Object.assign(moeda, moedaObj);
+      this.moedas.push(moeda);
+    }
+    return this.moedas;
+  }
+
 }
