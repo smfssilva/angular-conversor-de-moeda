c4ab944432a67da56b9e54daf5804dec00292320
Silva <smfssilva@yahoo.com>
2021-09-05 22:52:37 -0300

    Implementando a diretiva de validacao de numero1/2

--- src/app/conversor/directives/numero.directive.ts
+++ src/app/conversor/directives/numero.directive.ts
@@ -1,10 +1,24 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[Numero]'
 })
 export class NumeroDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
 
+  @HostListener('keyup', ['$event'])
+  onKeyUp($event: any) {
+    let valor = $event.target.value;
+    let posDecimais = valor.indexOf('.');
+
+    valor = valor.replace(/[\D]/g, '');
+
+    if (posDecimais > 0) {
+      valor = valor.substr(0, posDecimais)+ '.' +
+      valor.substr(posDecimais);
+    }
+
+    $event.target.value = valor;
+  }
 }
