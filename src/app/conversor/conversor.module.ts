d5fb78310daded6d62caa04942476101726dd992
Silva <smfssilva@yahoo.com>
2021-09-05 22:37:39 -0300

    Criando uma diretiva de validacao de numero

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule } from "@angular/common/http";
 import { ConversorComponent } from "./components";
 import { MoedaService, ConversorService } from "./services";
 import { FormsModule } from '@angular/forms';
+import { NumeroDirective } from './directives';
 
 
 
 @NgModule({
   declarations: [
-    ConversorComponent
+    ConversorComponent,
+    NumeroDirective
   ],
   imports: [
     CommonModule,
