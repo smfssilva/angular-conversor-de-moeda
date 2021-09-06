df6775c1fee451d4da3edecdbfabb35daa88b529
Silva <smfssilva@yahoo.com>
2021-09-05 23:06:57 -0300

    Implementando a diretiva de validacao de numero2/2

--- src/app/conversor/directives/numero.directive.ts
+++ src/app/conversor/directives/numero.directive.ts
@@ -1,9 +1,19 @@
 import { Directive, HostListener, ElementRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+
 
 @Directive({
-  selector: '[Numero]'
+  selector: '[Numero]',
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: NumeroDirective,
+    multi: true
+  }]
 })
-export class NumeroDirective {
+export class NumeroDirective implements ControlValueAccessor {
+
+  onTouched: any;
+  onChange: any;
 
   constructor(private el: ElementRef) { }
 
@@ -20,5 +30,22 @@ export class NumeroDirective {
     }
 
     $event.target.value = valor;
+    this.onChange(valor);
   }
+
+  writeValue(value: any): void {
+    this.el.nativeElement.value = value;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    throw new Error('Method not implemented.');
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
 }
