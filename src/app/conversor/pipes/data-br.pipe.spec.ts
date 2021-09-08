7e6d2bbc8a9b73600f50273192c58c98baab2b44
Silva <smfssilva@yahoo.com>
2021-09-08 00:42:20 -0300

    Implementando o filtro/pipe de formatacao de data

--- src/app/conversor/pipes/data-br.pipe.spec.ts
+++ src/app/conversor/pipes/data-br.pipe.spec.ts
@@ -5,4 +5,9 @@ describe('DataBrPipe', () => {
     const pipe = new DataBrPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('deve formatar a data 2017-03-31 para 31/03/2017', () => {
+    const pipe = new DataBrPipe();
+    expect(pipe.transform('2017-03-31')).toEqual('31/03/2017');
+  });
 });
--- src/app/conversor/pipes/data-br.pipe.ts
+++ src/app/conversor/pipes/data-br.pipe.ts
@@ -1,12 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'dataBr'
+  name: 'dataBr',
 })
 export class DataBrPipe implements PipeTransform {
+  transform(dataEn: string): string {
+    if (!dataEn) {
+      return '';
+    }
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    const dataArr = dataEn.split('-');
+    if (dataArr.length !== 3) {
+      return dataEn;
+    }
+    return dataArr[2] + '/' + dataArr[1] + '/' + dataArr[0];
   }
-
 }
